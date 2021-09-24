21f9944ec2e8e811c9fc4d50dfaf60b19bb6538c
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-09-24 12:20:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+# vkládání řádku do tabulky
+id INT UNSIGNED AUTO_INCREMENT,
+INSERT INTO car (id, manufacturer, model),
+VALUES (2,"Mercedes", "E63 AMG"),
+;
+
+#Zjištění id
+SELECT *
+FROM CAR
+
+#zrušení tabulky
+DROP TABLE car;
+
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('1', 'Ford', 'Mustang');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('2', 'Mercedes', 'E63 AMG');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('3', 'Mercedes', 'GT R');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('4', 'Mercedes', 'GT S');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('5', 'Mercedes Benz', 'GLS');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('6', 'Mercedes Benz', 'A 45');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('7', 'Mercedes Benz', 'GLE');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('8', 'Mercedes Benz', 'G');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('9', 'Mercedes Benz', 'CLS');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('10', 'Mercedes Benz', 'CLA');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('11', 'Mercedes Benz', 'C 63');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('12', 'Tesla', 'Model S');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('13', 'Tesla', 'Model 3');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('14', 'Tesla', 'Model X');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('15', 'Tesla', 'Model Y');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('16', 'Tesla', 'CyberTruck');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('17', 'BMW', 'E60');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('18', 'BMW', 'M3');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('19', 'BMW', 'M4');
+
+INSERT INTO car (id, manufacturer, model)
+VALUES ('20', 'BMW', 'M5');
+
